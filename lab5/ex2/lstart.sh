44b21f3cfaf3c60dfd9b6369eb5b593d35ce9049
michelegranatiero <mikigranatiero@hotmail.com>
2023-03-16 21:20:35 +0100

    lab5ex2 Xresources

--- lab5/ex2/lstart.sh
+++ lab5/ex2/lstart.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 xrdb ../../.XDefaults
+xrdb ../../.Xresources
 sudo kathara lstart --privileged
 for p in pc1 pc2 
 do
   xterm -e bash -c "kathara connect $p" &
 done
 xrdb ../../.XDefaults.alt
+xrdb ../../.Xresources
 for r in r1 r2
 do
   xterm -e bash -c "kathara connect $r" &
